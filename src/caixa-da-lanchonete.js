9af799ca848c5a10aa6b14bab80493d7c75b3293
Bruna Soncini <brusoncini@gmail.com>
2023-08-11 16:21:03 -0300

    erro quando o carrinho está  vazio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,87 +1,104 @@
 class CaixaDaLanchonete {
   calcularValorDaCompra(metodoDePagamento, itens) {
-    //cardapio
+    // cardapio
     const cardapio = [
       {
-        item: cafe,
+        item: "cafe",
         valor: 3.0,
+        itemExtra: false,
       },
+
       {
-        item: chantily,
+        item: "chantily",
         valor: 1.5,
+        itemExtra: true,
       },
       {
-        item: suco,
+        item: "suco",
+        itemExtra: false,
         valor: 6.2,
       },
+
       {
-        item: sanduiche,
+        item: "sanduiche",
         valor: 6.5,
+        itemExtra: false,
       },
+
       {
-        item: queijo,
+        item: "queijo",
         valor: 2.0,
+        itemExtra: true,
       },
+
       {
-        item: salgado,
+        item: "salgado",
         valor: 7.25,
+        itemExtra: false,
       },
+
       {
-        item: combo1,
+        item: "combo1",
         valor: 9.5,
+        itemExtra: false,
       },
+
       {
-        item: combo2,
+        item: "combo2",
         valor: 7.5,
+        itemExtra: false,
       },
     ];
 
-    // formas de pagamento válidas
-    const formasDePagamento = ["credito", "debito", "dinheiro"];
+    let valorTotal = 0;
 
-    for (let pagamento of formasDePagamento) {
-      if (metodoDePagamento !== formasDePagamento) {
-        return "Forma de pagamento inválida!";
-      }
-    }
-
-    //trata a entrada
+    // trata a entrada
     for (let item of itens) {
-      item.split(",");
-    }
+      const [produto, quantidade] = item.split(",");
 
-    const produto = itens[0];
-    const quantidade = Number(itens[1]);
+      // verifica se o item é valido
+      let produtoEncontrado = cardapio.find((produto) => {
+        return cardapio.item === produto;
+      });
 
-    // valor total dos itens
-    let valorTotal;
+      if (!produtoEncontrado) {
+        return "Item inválido!";
+      }
 
-    for (let compra of compras) {
+      // valor total dos itens
+
       valorTotal = produto * quantidade;
     }
 
-    //aplica descontos e taxas
-    if (metodoDePagamento === dinheiro) {
+    // verifica se o pagamento é valido
+    if (
+      metodoDePagamento !== "dinheiro" &&
+      metodoDePagamento !== "credito" &&
+      metodoDePagamento !== "debito"
+    ) {
+      return "Forma de pagamento inválida!";
+    }
+
+    // descontos e taxas
+    if (metodoDePagamento === "dinheiro") {
       valorTotal = valorTotal - valorTotal * 0.05;
-    } else if (metodoDePagamento === credito) {
+    } else if (metodoDePagamento === "credito") {
       valorTotal = valorTotal + valorTotal * 0.03;
     }
 
-    //"Item extra não pode ser pedido sem o principal".
-
-    // "Não há itens no carrinho de compra!"
-    if (!itens) {
-      return "Não há itens no carrinho de compra!";
+    // "Item extra não pode ser pedido sem o principal".
+    if (cardapio.itemExtra === true) {
     }
 
-    // "Quantidade inválida!"
-    if (itens.length === 0) {
+    // Se não forem pedidos itens
+    if (itens === 0) {
       return "Quantidade inválida!";
     }
 
-    // "Item inválido!"
-
-    // "Forma de pagamento inválida!"
+    // Se a quantidade de itens for zero
+    if (itens.length === 0) {
+      return "Não há itens no carrinho de compra!";
+    }
 
     return `R$ ${valorTotal.toFixed(2)}`;
   }
